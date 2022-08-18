3b0e8db6c1976eb816c99f0770e64366d07d632f
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-08-18 06:38:56 +0200

    feat: chat template looks nice

--- src/components/chat/ChatRoom.vue
+++ src/components/chat/ChatRoom.vue
@@ -1,15 +1,61 @@
 <template>
-  <v-container>
-    <v-row
-      v-for="(message, index) in messages"
-      :key="index"
-      class="pb-5"
-      :justify="checkIfLoggedInUser(message.senderName) ? 'end' : 'start'"
-    >
-    <v-avatar v-if="!checkIfLoggedInUser(message.senderName)" color="blue" size="32">IN</v-avatar>
-      <v-chip class="mr-5 ml-3">
-        {{ message.content }}
-      </v-chip>
+  <v-container fluid class="ml-5">
+    <div class="description">
+      <v-avatar class="mr-5 mb-2" color="indigo accent-1" size="40px"
+        ><span class="avatar">{{ getConnectionInitials() }}</span></v-avatar
+      >{{ connectionName }}
+    </div>
+    <v-container fluid class="scrollable-list">
+      <v-row
+        v-for="(message, index) in getMessages()"
+        :key="index"
+        class="pb-5"
+        :justify="checkIfLoggedInUser(message.senderName) ? 'end' : 'start'"
+      >
+        <v-avatar
+          v-if="!checkIfLoggedInUser(message.senderName)"
+          color="indigo accent-1"
+          size="40"
+          ><span class="avatar">{{ getConnectionInitials() }}</span></v-avatar
+        >
+        <!-- <v-chip :color="checkIfLoggedInUser(message.senderName) ? '#c1caff' : 'light-grey'" class="mr-5 ml-3 message-chip my-font">
+          <span>
+            {{ message.content }}
+          </span>
+        </v-chip> -->
+        <span
+          v-if="checkIfLoggedInUser(message.senderName)"
+          class="mr-5 ml-3 custom-message-chip my-font user-chip"
+        >
+          {{ message.content }}
+        </span>
+        <span
+          v-else
+          class="mr-5 ml-3 custom-message-chip my-font connection-chip"
+        >
+          {{ message.content }}
+        </span>
+      </v-row>
+    </v-container>
+    <v-spacer />
+    <v-row no-gutters dense class="mt-2">
+      <v-col cols="11">
+        <v-textarea
+          rows="1"
+          v-model="message"
+          @keydown.prevent.enter="sendMessage()"
+          class="chat-field"
+          filled
+          rounded
+          no-resize
+        >
+        </v-textarea>
+      </v-col>
+      <v-col cols="1">
+        <v-btn fab color="indigo accent-1" @click="sendMessage()"
+          ><v-icon class="send-icon">mdi-send</v-icon></v-btn
+        >
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -17,37 +63,160 @@
 <script>
 export default {
   name: "ChatRoom",
+  props: {
+    chatId: String,
+    connectionName: String,
+  },
   data() {
     return {
+      message: "",
       messages: [
         {
+          chatId: 1,
           senderName: "Natasa",
           recipientName: "Vera",
           content: "Neka poruka test 1",
         },
         {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content: "Neka poruka test 2Neka poruka test 2",
+        },
+        {
+          chatId: 1,
+          senderName: "Natasa",
+          recipientName: "Vera",
+          content: "Neka poruka test 3Neka poruka test 2Neka poruka test 2",
+        },
+        {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content:
+            "Neka poruka test 4Neka poruka test 2Neka poruka test 2Neka poruka test 2Neka poruka test 2Neka poruka test 2Neka poruka test 2",
+        },
+        {
+          chatId: 1,
           senderName: "Vera",
           recipientName: "Natasa",
           content: "Neka poruka test 2",
         },
         {
+          chatId: 1,
           senderName: "Natasa",
           recipientName: "Vera",
           content: "Neka poruka test 3",
         },
         {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content: "Neka poruka test 4",
+        },
+        {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content: "Neka poruka test 2",
+        },
+        {
+          chatId: 1,
+          senderName: "Natasa",
+          recipientName: "Vera",
+          content: "Neka poruka test 3",
+        },
+        {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content: "Neka poruka test 4",
+        },
+        {
+          chatId: 1,
+          senderName: "Vera",
+          recipientName: "Natasa",
+          content: "Neka poruka test 2",
+        },
+        {
+          chatId: 1,
+          senderName: "Natasa",
+          recipientName: "Vera",
+          content: "Neka poruka test 3",
+        },
+        {
+          chatId: 1,
           senderName: "Vera",
           recipientName: "Natasa",
           content: "Neka poruka test 4",
         },
       ],
-    } 
+    };
   },
   methods: {
-    checkIfLoggedInUser: function(name) {
-        return name == 'Natasa';
-    }
-  }
+    checkIfLoggedInUser: function (name) {
+      return name == "Natasa";
+    },
+    getMessages: function () {
+      return this.messages.filter((x) => x.chatId === this.chatId);
+    },
+    getConnectionInitials() {
+      return this.connectionName.split(" ").reduce((p, c) => p + c[0], "");
+    },
+    sendMessage() {
+      if (this.message === "") {
+        return;
+      }
+      alert("sent message: " + this.message);
+      this.message = "";
+    },
+  },
 };
 </script>
-<style lang=""></style>
+<style scoped>
+.description {
+  font-family: "Baloo2", Helvetica, Arial;
+  font-size: 40px;
+  font-weight: 500;
+}
+.avatar {
+  font-family: "Baloo2", Helvetica, Arial;
+  font-size: 16px;
+  margin-top: 1px;
+  font-weight: 700;
+}
+
+.scrollable-list {
+  overflow-y: auto;
+  max-height: calc(100vh - 430px);
+}
+
+.chat-field {
+  margin-right: 12px;
+  overflow: auto;
+}
+
+.send-icon {
+  max-height: 12px;
+  max-width: 12px;
+}
+
+.custom-message-chip {
+  border-radius: 24px;
+  padding: 8px;
+  min-height: 40px;
+  max-width: 400px;
+}
+
+.user-chip {
+  background-color: #c1caff;
+}
+
+.connection-chip {
+  background-color: #ebebeb;
+}
+
+.test {
+  color: #c1caff;
+}
+</style>
--- src/components/chat/ConnectionList.vue
+++ src/components/chat/ConnectionList.vue
@@ -1,22 +1,31 @@
 <template>
-    <v-list two-line class="ml-3 list-overflow" max-height="700px">
-      <template v-for="(item, index) in items">
-          <v-list-item
+  <v-container fluid class="list-container">
+    <div class="description ml-4 mt-2">Chat</div>
+      <v-list two-line class="pr-5 scrollable-list">
+        <template v-for="(item, index) in items">
+          <router-link
             :key="index"
-            ripple
+            :to="{
+              name: 'ChatRoom',
+              params: { chatId: item.id, connectionName: item.connectionName },
+            }"
+            v-slot="{ navigate }"
           >
-            <v-list-item-avatar>
-              <img :src="item.avatar" />
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title v-html="item.title"></v-list-item-title>
-              <v-list-item-subtitle
-                v-html="item.subtitle"
-              ></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-      </template>
-    </v-list>
+            <v-list-item ripple @click="navigate">
+              <v-list-item-avatar>
+                <img :src="item.avatar" />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-html="item.subtitle"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+        </template>
+      </v-list>
+  </v-container>
 </template>
 <script>
 export default {
@@ -25,41 +34,52 @@ export default {
     return {
       items: [
         {
+          id: 1,
+          connectionName: "Vera Kovacevic",
           avatar: "https://picsum.photos/250/300?image=660",
           title: "Meeting @ Noon",
           subtitle: `<span class="font-weight-bold">Spike Lee</span> &mdash; I'll be in your neighborhood`,
         },
         {
+          id: 2,
+          connectionName: "Mario Kujundzic",
           avatar: "https://picsum.photos/250/300?image=821",
           title: 'Summer BBQ <span class="grey--text text--lighten-1"></span>',
           subtitle:
             '<span class="font-weight-bold">to Operations support</span> &mdash; Wish I could come.',
         },
         {
+          id: 3,
+          connectionName: "Marija Radovic",
           avatar: "https://picsum.photos/250/300?image=783",
           title: "Yes yes",
           subtitle:
             '<span class="font-weight-bold">Bella</span> &mdash; Do you have Paris recommendations',
         },
         {
+          id: 4,
+          connectionName: "Ana Cvetkovic",
           avatar: "https://picsum.photos/250/300?image=1006",
           title: "Dinner tonight?",
           subtitle:
             '<span class="font-weight-bold">LaToya</span> &mdash; Do you want to hang out?',
         },
         {
+          id: 5,
           avatar: "https://picsum.photos/250/300?image=146",
           title: "So long",
           subtitle:
             '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
         },
         {
+          id: 6,
           avatar: "https://picsum.photos/250/300?image=1008",
           title: "Breakfast?",
           subtitle:
             '<span class="font-weight-bold">LaToya</span> &mdash; Do you want to hang out?',
         },
         {
+          id: 7,
           avatar: "https://picsum.photos/250/300?image=839",
           title:
             'Winter Porridge <span class="grey--text text--lighten-1"></span>',
@@ -67,35 +87,40 @@ export default {
             '<span class="font-weight-bold">cc: Daniel</span> &mdash; Tell me more...',
         },
         {
+          id: 8,
           avatar: "https://picsum.photos/250/300?image=145",
           title: "Oui oui",
           subtitle:
             '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
         },
         {
+          id: 9,
           avatar: "https://picsum.photos/250/300?image=145",
           title: "Oui oui",
           subtitle:
             '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
         },
         {
+          id: 10,
           avatar: "https://picsum.photos/250/300?image=145",
           title: "Oui oui",
           subtitle:
             '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
         },
-        {
-          avatar: "https://picsum.photos/250/300?image=145",
-          title: "Oui oui",
-          subtitle:
-            '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
-        },
-        {
-          avatar: "https://picsum.photos/250/300?image=145",
-          title: "Oui oui",
-          subtitle:
-            '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
-        },
+        // {
+        //   id: 11,
+        //   avatar: "https://picsum.photos/250/300?image=145",
+        //   title: "Oui oui",
+        //   subtitle:
+        //     '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
+        // },
+        // {
+        //   id: 12,
+        //   avatar: "https://picsum.photos/250/300?image=145",
+        //   title: "Oui oui",
+        //   subtitle:
+        //     '<span class="font-weight-bold">Nancy</span> &mdash; Do you see what time it is?',
+        // },
       ],
     };
   },
@@ -103,9 +128,13 @@ export default {
 </script>
 
 <style scoped>
-
-.list-overflow {
-    overflow-y: scroll;
+.list-container {
+  padding: 0 0 0 0;
+  height: 100%;
 }
 
+.scrollable-list {
+  overflow-y: auto;
+  max-height: calc(100vh - 369px);
+}
 </style>
