274e88e77c90371ff5a0a2f51fcd1cf841375a6c
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2022-08-20 20:10:46 +0200

    feat: user details connected with backend

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -84,7 +84,6 @@
 const apiURL = "auth-service/authentication/login";
 export default {
   name: "LoginForm",
-
   data() {
     return {
       user: {
@@ -100,7 +99,6 @@ export default {
       loading: false,
     };
   },
-
   methods: {
     login: function () {
       this.loading = true;
@@ -112,32 +110,26 @@ export default {
       this.axios({
         url: apiURL,
         method: "POST",
-        data: this.user
-      }).then((response) => {
-        let loggedInUser = {
-          id: response.data.id,
-          role: response.data.role,
-          token: response.data.token
-        }
-        localStorage.setItem("role", response.data.role);
-        localStorage.setItem(
-          "authKey",
-          "Bearer " + response.data.token
-        );
-        localStorage.setItem("user", JSON.stringify(loggedInUser));
-        this.axios.defaults.headers["Authorization"] = "Bearer " + response.data.token;
-        this.loading = false;
-        this.$router.push({ name: "HomeView" });
-      }).catch((error) => {
-        this.loading = false;
-        if (error.response.status === 401) {
-          this.$root.snackbar.error("Invalid credentials");
-        } else {
-          this.$root.snackbar.error(error.response.data.message);
-        }
-        this.$refs.form.reset();
+        data: this.user,
       })
-      
+        .then((response) => {
+          localStorage.setItem("id", response.data.id);
+          localStorage.setItem("role", response.data.role);
+          localStorage.setItem("token", response.data.token);
+          this.axios.defaults.headers["Authorization"] =
+            "Bearer " + response.data.token;
+          this.loading = false;
+          this.$router.push({ name: "HomeView" });
+        })
+        .catch((error) => {
+          this.loading = false;
+          if (error.response.status === 400) {
+            this.$root.snackbar.error("Invalid credentials");
+          } else {
+            this.$root.snackbar.error(error.response.data.message);
+          }
+          this.$refs.form.reset();
+        });
     },
   },
 };
@@ -147,7 +139,6 @@ export default {
 .description {
   font-family: "Baloo2", Helvetica, Arial;
 }
-
 .main-screen {
   background: rgb(181, 136, 251);
   background: radial-gradient(
